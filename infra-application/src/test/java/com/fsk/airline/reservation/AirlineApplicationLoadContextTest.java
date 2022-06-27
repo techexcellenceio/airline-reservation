49db991c7510d256d69067adf421eb38b6b46ebb
StanKluk <stanislas.klukowski@gmail.com>
2022-06-27 13:00:48 +0200

    initial spring boot configuration

---
+++ infra-application/src/test/java/com/fsk/airline/reservation/AirlineApplicationLoadContextTest.java
@@ -0,0 +1,12 @@
+package com.fsk.airline.reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlineApplicationLoadContextTest {
+
+	@Test
+	void contextLoads() {
+	}
+}
