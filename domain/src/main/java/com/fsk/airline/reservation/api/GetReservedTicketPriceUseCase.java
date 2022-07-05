cb0997ad09e13bd9f484638eb95b61de2e73a1f9
StanKluk <stanislas.klukowski@gmail.com>
2022-07-05 16:08:15 +0200

    use cases can be functional interfaces

--- domain/src/main/java/com/fsk/airline/reservation/api/GetReservedTicketPriceUseCase.java
+++ domain/src/main/java/com/fsk/airline/reservation/api/GetReservedTicketPriceUseCase.java
@@ -4,7 +4,7 @@ import com.fsk.airline.reservation.model.TicketNumber;
 
 import java.math.BigDecimal;
 
+@FunctionalInterface
 public interface GetReservedTicketPriceUseCase {
-
 	BigDecimal getReservedTicketPrice(String customerLogin, TicketNumber ticketNumber);
 }
--- domain/src/main/java/com/fsk/airline/reservation/api/SearchReservedTicketUseCase.java
+++ domain/src/main/java/com/fsk/airline/reservation/api/SearchReservedTicketUseCase.java
@@ -4,7 +4,7 @@ import com.fsk.airline.reservation.model.ReservedTicket;
 import com.fsk.airline.reservation.model.TicketNumber;
 
 import java.util.Optional;
-
+@FunctionalInterface
 public interface SearchReservedTicketUseCase {
 
 	Optional<ReservedTicket> findReservedTicket(String customerLogin, TicketNumber ticketNbr);
