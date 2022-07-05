0b91ace6397d2e09b2c8c3aa1c234d8213b93a19
StanKluk <stanislas.klukowski@gmail.com>
2022-07-05 17:18:29 +0200

    logging if exceptions are thrown to controllers

--- infra-application/src/main/java/com/fsk/airline/reservation/controller/GeneralErrorController.java
+++ infra-application/src/main/java/com/fsk/airline/reservation/controller/GeneralErrorController.java
@@ -1,16 +1,22 @@
 package com.fsk.airline.reservation.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 
 @ControllerAdvice
 public class GeneralErrorController {
 
+	private final Logger logger = LoggerFactory.getLogger(GeneralErrorController.class);
+
 	@ExceptionHandler(IllegalArgumentException.class)
-	protected ResponseEntity<ErrorDto> handleException(IllegalArgumentException ex) {
+	protected ResponseEntity<ErrorDto> handleException(IllegalArgumentException ex, WebRequest webRequest) {
+		logger.error("Error in request " + webRequest.getDescription(false), ex);
 		ErrorDto errorDto = new ErrorDto(ex.getMessage());
 		return new ResponseEntity<>(errorDto, new HttpHeaders(), HttpStatus.BAD_REQUEST);
 	}
